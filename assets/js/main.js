c0271534ebe6a5b984ed1d429a2c5f0aa2b58713
Sylvain Bauvais <sylvain.bauvais@hova.fr>
2023-10-10 17:58:19 +0200

    Ajout d'animations au scroll pour les images de la homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,38 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Get references to the button and navigation menu
-  var menuButton = document.querySelector(".menu");
-  var navigationMenu = document.querySelector(".navigation");
+document.addEventListener("DOMContentLoaded", () => {
+  // Constants for selectors
+  const menuButton = document.querySelector(".menu");
+  const navigationMenu = document.querySelector(".navigation");
+  const navLinks = document.querySelectorAll(".nav-item a");
+  const scrollAnimations = document.querySelectorAll(".scroll-animation");
 
   // Function to toggle the navigation menu
   function toggleMenu() {
-    // Toggle the "hidden" attribute on the navigation menu
     navigationMenu.hidden = !navigationMenu.hidden;
-
-    // Toggle the "aria-expanded" attribute on the button for accessibility
-    var isMenuHidden = navigationMenu.hidden;
+    const isMenuHidden = navigationMenu.hidden;
     menuButton.setAttribute("aria-expanded", isMenuHidden ? "false" : "true");
+  }
+
+  // Function to handle scroll animations
+  function handleScrollAnimation() {
+    scrollAnimations.forEach((element) => {
+      const elementPosition = element.getBoundingClientRect().top;
+      const screenHeight = window.innerHeight;
+      if (elementPosition < screenHeight * 0.85) {
+        element.style.opacity = "1";
+        element.style.transform = "translateY(0)";
+      }
+    });
   }
 
   // Toggle the navigation menu when the button is clicked
   menuButton.addEventListener("click", toggleMenu);
 
   // Add event listener to close the menu when a link is clicked
-  var navLinks = document.querySelectorAll(".nav-item a");
-  navLinks.forEach(function(link) {
+  navLinks.forEach((link) => {
     link.addEventListener("click", toggleMenu);
   });
+
+  // Check for scroll animations when the page loads and on scroll
+  window.addEventListener("scroll", handleScrollAnimation);
+  handleScrollAnimation();
 });
